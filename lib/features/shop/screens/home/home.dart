4e8e1d46a03e7c01a7d9553fd0220bd4d6488d22
IbrahimuJohn · he/him <ibrahimjhn255@gmail.com>
2025-02-07 19:39:04 +0300

    Fifth Commit

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:jhn_store/common/widgets/custom_shape/curved_edges/circular_container.dart';
+import 'package:jhn_store/utils/constants/colors.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              color: JColors.primary,
+              padding: const EdgeInsets.all(0),
+              child: Stack(
+                children: [
+                  Positioned(
+                    top: -150,
+                    right: -250,
+                    child: JCircularContainer(
+                      backgroundColor: JColors.textWhite.withOpacity(0.1),
+                    ),
+                  ),
+                  Positioned(
+                    top: 100,
+                    right: -300,
+                    child: JCircularContainer(
+                      backgroundColor: JColors.textWhite.withOpacity(0.1),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
